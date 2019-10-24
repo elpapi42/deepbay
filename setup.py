f3c95c4495f73670bd5bb92c68cc3143663015ad
ElPapi42 <whitman-2@hotmail.com>
2019-10-24 18:10:20 -0400

    Added DeepFeedFowrd Model

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ import setuptools
 with open("README.md", "r") as fh:
 	long_description = fh.read()
 
+version="v0.2"
+
 setuptools.setup(
 	name="deepbay",
-	version="v0.1",
+	version=version,
 	author="Whitman Bohorquez",
 	author_email="whitman-2@hotmail.com",
 	description="Tensorflow/Keras Plug-N-Play Deep Learning Models Compilation",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/ElPapi42/DeepBay",
-	download_url = '',
+	download_url = "https://github.com/ElPapi42/DeepBay/archive/" + version + ".tar.gz",
 	keywords = ['Keras', 'Model', 'Plug And Play', "Tensorflow"],
 	packages=setuptools.find_packages(),
 	install_requires=[
