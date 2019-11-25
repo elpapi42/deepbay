4c50e780c7e34fc06279ddeb417dc1d50d08e126
ElPapi42 <whitman-2@hotmail.com>
2019-11-25 18:53:03 -0400

    Update Dense Block Test
    This test is not reliable and must be changed or updated
    will commit this for place a basement for CI/CD

--- tests/test_dense_block.py
+++ tests/test_dense_block.py
@@ -2,22 +2,36 @@ import tensorflow as tf
 import numpy as np
 import pytest
 
-from DeepBay import DenseBlock
+from deepbay import DenseBlock
 
-def test_dense_block_call():
-    # Set seeds for reproducibility
+# This test is not reliable
+def test_dense_block_weights_update():
+    # seeds the NRG
     tf.random.set_seed(156)
 
-    # I/O
+    # testing I/O
     test_input = np.array([[1.0, 1.0, 0.5], [0.1, -1.0, -0.5]])
-    test_output = np.array([[0.14267592], [0.23824959]])
+    test_output = np.array([[0.94], [0.15]])
 
     # Intantiate a dense block
-    dense = DenseBlock(1)
-    dense.compile(
+    block = DenseBlock(units=1, use_batch_norm=True)
+    block.compile(
         tf.keras.optimizers.Adam(),
         tf.keras.losses.MeanSquaredError(),
     )
-    dense.train_on_batch(test_input, np.array([[0.0], [0.0]]))
+    # Init parameters
+    block.train_on_batch(test_input, test_output)
 
-    assert tf.equal(dense(test_input), test_output).numpy().all()
+    # Recor initial parameters
+    initial_params = block.dense.get_weights()
+
+    # Trin by one step
+    block.train_on_batch(test_input, test_output)
+
+    # Record final parameters
+    final_params = block.dense.get_weights()
+
+    #Check if the parameters was actually updated
+    was_updated = (initial_params[0] != final_params[0]).all() and (initial_params[1] != final_params[1]).all()
+
+    assert was_updated
